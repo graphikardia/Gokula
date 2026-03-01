0ad632b8863f458224754326976d7d22bc5a9249
Graphikardia <graphikardia@gmail.com>
2026-03-01 21:27:31 +0530

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-12 text-center text-gray-400">
+      © {new Date().getFullYear()} Geetha Gokula — Built with ❤️
+    </footer>
+  )
+}
